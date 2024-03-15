f7791fdd894abf0cedc09b0a9547e2cac12a98b9
koustav2 <koustavm.aeie208@nsec.ac.in>
2024-03-16 00:02:02 +0530

    img generator with ai

---
+++ backend/openAi/img_generator.js
@@ -0,0 +1,43 @@
+const { OpenAI } = require('openai');
+const express = require('express');
+
+
+const router = express.Router();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+router.post('/generate-image', async (req, res) => {
+    const { prompt, size } = req.body;
+
+    const imageSize =
+        size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+    try {
+        const options = ["vivid", "natural"]
+        const randomIndex = Math.floor(Math.random() * options.length);
+        const response = await openai.images.generate({
+            prompt: prompt,
+            model: "dall-e-3",
+            response_format: 'url',
+            size: "1024x1024",
+            style: options[randomIndex],
+            quality: "hd"
+
+        })
+        const imageUrl = response.data.map(elm => elm.url)
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl[0],
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: 'The image could not be generated',
+        });
+    }
+})
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config(".env");
+const express = require('express');
+const app = express();
+const cors = require('cors'); 
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON bodies
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/v1",require("./openAi/img_generator"))
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
