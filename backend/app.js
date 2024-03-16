2480f76c6987fda3e290452f9ed9ec9dbcf1a14e
koustav2 <koustavm.aeie208@nsec.ac.in>
2024-03-16 10:29:21 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,17 @@ require('dotenv').config(".env");
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON bodies
 app.use(cors({
   credentials: true
 }))
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"))
+app.use(cookieParser());
 
 app.use("/api/v1", require("./openAi/img_generator"))
 app.listen(PORT, () => {
