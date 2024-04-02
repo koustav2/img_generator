9fa34400f876f53d44cf72ce0205da71fbf340da
koustav2 <koustavm.aeie208@nsec.ac.in>
2024-04-03 00:03:53 +0530

    commit

--- frontend/src/utils/generateImg.js
+++ frontend/src/utils/generateImg.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 
 
-const generateImg = (prompt) => {
+const generateImg = async (prompt) => {
     // Return axios post request as Promise
-    return axios.post(
-        `${import.meta.env.VITE_SERVER_URL}/api/v1/generate-image`,
-        { prompt }
-      )
-      .then(response => response.data) 
-      .catch(err => {
-        console.error(err);
-        throw err; // Re-throw to make sure Promise ends up in 'rejected' state
-      });
+    try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_SERVER_URL}/api/v1/generate-image`,
+      { prompt }
+    );
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw err; // Re-throw to make sure Promise ends up in 'rejected' state
+  }
   };
 
 export default generateImg;
